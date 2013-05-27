27556bc8b5431c8305e445232d742443a43130e3
TharinduDG <tdgalappaththi@gmail.com>
2013-05-28 02:19:59 +0530

    File listing feature modifications

--- cloud_to_cloud/templates/filesList.php
+++ cloud_to_cloud/templates/filesList.php
@@ -67,6 +67,7 @@ if (\OC\Files\Filesystem::isSharable($dir . '/')) {
 
 // Load the files we need
 OCP\Util::addStyle('files', 'files');
+OCP\Util::addStyle('cloud_to_cloud', 'custom');
 OCP\Util::addscript('files', 'jquery.iframe-transport');
 OCP\Util::addscript('files', 'jquery.fileupload');
 OCP\Util::addscript('files', 'jquery-visibility');
@@ -77,7 +78,7 @@ OCP\Util::addscript('files', 'keyboardshortcuts');
 
 OCP\App::setActiveNavigationEntry('files_index');
 
-$tmpl = new OCP\Template('files', 'part.list', '');
+$tmpl = new OCP\Template('cloud_to_cloud', 'part.list', '');
 $tmpl->assign('files', $files);
 $tmpl->assign('permissions', $permissions);
 $tmpl->assign('baseURL', OCP\Util::linkTo('cloud_to_cloud', 'viewShared.php') . '?dir=');
@@ -85,48 +86,24 @@ $tmpl->assign('downloadURL', OCP\Util::linkToRoute('download', array('file' => '
 $tmpl->assign('disableSharing', false);
 $files_list = $tmpl->fetchPage();
 
-$allowZipDownload = intval(OCP\Config::getSystemValue('allowZipDownload', true));
 
 
 ?>
 <div id="controls">
-<?php print_unescaped($breadcrumbNav->fetchPage()); ?>
+	<?php print_unescaped($breadcrumbNav->fetchPage()); ?>
+	<div id="moto">
+		Cloud to Cloud : Share files between clouds
+	</div>
 </div>
 <table id="filestable">
 	<thead>
 		<tr>
 			<th id='headerName'>
-				<input type="checkbox" id="select_all" />
 				<span class='name'><?php p($l->t( 'Name' )); ?></span>
-				<span class='selectedActions'>
-					<?php if($allowZipDownload) : ?>
-						<a href="" class="download">
-							<img class="svg" alt="Download"
-								 src="<?php print_unescaped(OCP\image_path("core", "actions/download.svg")); ?>" />
-							<?php p($l->t('Download'))?>
-						</a>
-					<?php endif; ?>
-				</span>
 			</th>
 			<th id="headerSize"><?php p($l->t( 'Size' )); ?></th>
 			<th id="headerDate">
 				<span id="modified"><?php p($l->t( 'Modified' )); ?></span>
-				<?php if ($permissions & OCP\PERMISSION_DELETE): ?>
-<!-- 					NOTE: Temporary fix to allow unsharing of files in root of Shared folder -->
-					<?php if ($_['dir'] == '/Shared'): ?>
-						<span class="selectedActions"><a href="" class="delete-selected">
-							<?php p($l->t('Unshare'))?>
-							<img class="svg" alt="<?php p($l->t('Unshare'))?>"
-								 src="<?php print_unescaped(OCP\image_path("core", "actions/delete.svg")); ?>" />
-						</a></span>
-					<?php else: ?>
-						<span class="selectedActions"><a href="" class="delete-selected">
-							<?php p($l->t('Delete'))?>
-							<img class="svg" alt="<?php p($l->t('Delete'))?>"
-								 src="<?php print_unescaped(OCP\image_path("core", "actions/delete.svg")); ?>" />
-						</a></span>
-					<?php endif; ?>
-				<?php endif; ?>
 			</th>
 		</tr>
 	</thead>
