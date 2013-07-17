f6fc738feb5493dd093dd97c741d7a04f65f0777
TharinduDG <tdgalappaththi@gmail.com>
2013-07-17 21:53:47 +0530

    Testing code

--- cloud_to_cloud/mail.php
+++ cloud_to_cloud/mail.php
@@ -1,12 +1,12 @@
 <?php
 
-//\OCP\Util::sendMail("tdgalappaththi@gmail.com","tdg","subject","body","owncloud@abc.com","owncloud5");
-//
-//\OCA\CloudToCloud\CloudToCloudShare::sendMail("tdgalappaththi@gmail.com","tdg","abc","sfd");
+// //\OCP\Util::sendMail("tdgalappaththi@gmail.com","tdg","subject","body","owncloud@abc.com","owncloud5");
+// //
+// //\OCA\CloudToCloud\CloudToCloudShare::sendMail("tdgalappaththi@gmail.com","tdg","abc","sfd");
 
-\OC_Util::setupFS('admin5');
-$path = \OC\Files\Filesystem::getPath('124');
-$zip = new OC_Archive_ZIP($path);
-$zip->open($path, ZipArchive::CREATE);
-var_dump($zip->getFiles());
-var_dump($path);
+// \OC_Util::setupFS('admin5');
+// $path = \OC\Files\Filesystem::getPath('124');
+// $zip = new OC_Archive_ZIP($path);
+// $zip->open($path, ZipArchive::CREATE);
+// var_dump($zip->getFiles());
+// var_dump($path);
